c31aae3bf28456b71b09aec37b116f130b4ec23a
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-10-02 20:27:21 -0400

    created route for a POST request to store short and long urls in the database and to redirect user to the longURL

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ function generateRandomString() {
   let randomString = "";
   for (let i = 0; i < 6; i++) {
     const index = Math.floor(Math.random() * alphanumericValues.length);
-    randomString += alphanumericValues.charAt(index)
+    randomString += alphanumericValues.charAt(index);
   }
   return randomString;
 }
@@ -21,38 +21,36 @@ const urlDatabase = {
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello!")
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 })
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(`/urls/${randomString}`);
 });
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id]
+  res.redirect(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
-  console.log(req.params)
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
+  res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
-
 app.listen(PORT, () => {
-console.log(`Example app listening on port ${PORT}`)
+console.log(`Example app listening on port ${PORT}`);
 });
