6fabfd7a8fc61d4927ccff8830d1b81dd4264925
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-10-04 21:22:13 -0400

    add a function to authenticate user

--- helper_functions.js
+++ helper_functions.js
@@ -1,18 +1,3 @@
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
-
-let emailAddress = "user2@example.com"
-
 const getUserByEmail = function(users, emailAddress) {
   for (let user in users) {
     if (users[user].email === emailAddress) {
@@ -22,25 +7,22 @@ const getUserByEmail = function(users, emailAddress) {
   return null;
 };
 
-console.log(getUserByEmail(users, emailAddress))
-
 
 const authenticateUser = function(users, email, password) {
-  const user = getUserByEmail(users, email);
+  let user = getUserByEmail(users, email);
 
     if (!user) {
       return { error: "No user found", user: null}
     }
 
-    if(user.password === password) {
+    if(user.password !== password) {
       return { error: "Password doesn't match", user: null}
     }
 
     return { error: null, user }
 }
 
-const { error, user } = authenticateUser(arg1, arg2, arg3)
-
-if (error) {
-
+module.exports = {
+  getUserByEmail,
+  authenticateUser
 }
